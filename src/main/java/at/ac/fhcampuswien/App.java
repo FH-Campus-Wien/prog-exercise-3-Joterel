e38525bafa61104418c45e300ad603f76201ec15
Jullian Rendl <julian.rendl@gmx.at>
2022-10-20 12:46:46 +0200

    test

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -8,6 +8,7 @@ public class App {
     // Implement all methods as public static
 
     public static void main(String[] args) {
+        System.out.println();
         // test your method implementations here
         // make method calls
         // print their results
